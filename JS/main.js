ae11a304966c1c6291d2cb158d2debc5ddef9aed
Baldezin <valdirjuniorprincipal@gmail.com>
2024-05-15 15:55:51 -0300

    2º commit

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+function bravo(){
+    document.getElementById("muito").innerHTML = "Carinha Muito Brava D:<";
+    document.getElementById("normal").innerHTML = "Carinha Brava ):<";
+    document.getElementById("muito").className = "vermelho";
+    document.getElementById("normal").className = "vermelho";
+}
